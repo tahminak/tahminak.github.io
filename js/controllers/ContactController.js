8d386680647993e87c5353746ef45cd142fd2999
Tahmina Khan <tahmina.khan@gmail.com>
2015-02-26 15:57:43 -0500

    Updated Contact Page

--- js/controllers/ContactController.js
+++ js/controllers/ContactController.js
@@ -8,9 +8,42 @@ var app= angular.module("myResumeApp");
     
     
     
-var ContactController=function ($scope){
+var ContactController=function ($scope,$http){
     
-    //console.log("In About controller");
+    console.log("In Contact controller");
+    
+      // create a blank object to hold our form information
+      // $scope will allow this to pass between controller and view
+      $scope.formData = {};
+
+    
+    $scope.sendEmail=function (){
+        
+        console.log("Clicked the submit button");
+        
+        $http({
+  method  : 'POST',
+  url     : '/php/mail.php',
+  data    : $.param($scope.formData),  // pass in data as strings
+  headers : { 'Content-Type': 'application/x-www-form-urlencoded' }  // set the headers so angular passing info as form data (not request payload)
+ })
+  .success(function(data) {
+    console.log(data);
+//
+//    if (!data.success) {
+//      // if not successful, bind errors to error variables
+//      $scope.errorName = data.errors.name;
+//      $scope.errorSuperhero = data.errors.superheroAlias;
+//    } else {
+//      // if successful, bind success message to message
+//      $scope.message = data.message;
+//    }
+  });
+        
+         
+
+    }
+    
     
 }
     
--- php/mail.php
+++ php/mail.php
@@ -2,8 +2,8 @@
 <?php
 
     $from= $_POST["email"]; // sender
-    $name= $_POST["first_name"];
-    $message = $_POST["comments"];
+    $name= $_POST["name"];
+    $message = $_POST["message"];
     // message lines should not exceed 70 characters (PHP rule), so wrap it
     $message = wordwrap($message, 70);
     // send mail
